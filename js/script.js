14cdd91650589c2cb84145d5efec88b22af86d9a
Aprameya Gupta <aprameyagupta27@gmail.com>
2020-07-12 17:28:53 +0530

    SignUp Issue Resolve and Profile Page updated

--- js/script.js
+++ js/script.js
@@ -7,4 +7,15 @@ function darkMode() {
     document.body.classList.toggle("dark-mode");
 }
 
+function editBio() {
+  bio = document.getElementById('bio');
+  if(bio.disabled) {
+    bio.disabled = false;
+    return false;
+  } else {
+    bio.disabled = true;
+    return true;
+  }
+}
+
 // window.onload = () => alert("Logged In Successfully!");
